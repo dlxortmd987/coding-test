8bee8fcfa324b5872a40107e1c293d9c0fc28588
Taek Seung Lee <dlxortmd987@gmail.com>
2023-10-25 11:13:58 +0900

    코드트리 2023.10.25

--- code_tree/intermidiate_low/DP1/아이템을_적절히_고르는_문제/배낭_채우기.java
+++ code_tree/intermidiate_low/DP1/아이템을_적절히_고르는_문제/배낭_채우기.java
@@ -2,14 +2,15 @@ package code_tree.intermidiate_low.DP1.아이템을_적절히_고르는_문제;
 
 import java.util.Scanner;
 
-public class 부분_수열의_합 {
+public class 배낭_채우기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		int[] arr = new int[n];
+		int[][] arr = new int[n][2];
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
 		}
 		int[] dp = new int[m + 1];
 
@@ -18,20 +19,17 @@ public class 부분_수열의_합 {
 			dp[i] = Integer.MIN_VALUE;
 		}
 
-		for (int i = 1; i < n; i++) {
-			int num = arr[i];
-			for (int j = m; j > 0; j--) {
-				if (j >= num) {
-					if (dp[j - num] == Integer.MIN_VALUE)
-						continue;
-					dp[j] = Math.max(dp[i], dp[j - num] + 1);
-				}
+		for (int i = 0; i < n; i++) {
+			int num = arr[i][0];
+			for (int j = m; j >= num; j--) {
+				dp[j] = Math.max(dp[j], dp[j - num] + arr[i][1]);
 			}
 		}
 
-		if (dp[m] >= 1)
-			System.out.println("Yes");
-		else
-			System.out.println("No");
+		int res = 0;
+		for (int i = 0; i <= m; i++) {
+			res = Math.max(res, dp[i]);
+		}
+		System.out.println(res);
 	}
 }
