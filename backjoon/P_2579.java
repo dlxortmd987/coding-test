ae94b1e101a43f6c0b82e436ba76eeccb8fc203c
Taek Seung Lee <dlxortmd987@gmail.com>
2023-12-19 16:17:32 +0900

    백준 2023.12.19

---
+++ backjoon/P_2579.java
@@ -0,0 +1,42 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2579 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[][] dp = new int[n + 1][2];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		if (n < 2) {
+			System.out.println(arr[0]);
+			return;
+		} else if (n < 3) {
+			System.out.println(arr[0] + arr[1]);
+			return;
+		}
+
+		dp[0][0] = 0;
+		dp[1][0] = arr[0];
+		dp[1][1] = arr[0];
+		dp[2][0] = arr[1] + dp[1][0];
+		dp[2][1] = arr[1];
+
+		for (int i = 3; i < n + 1; i++) {
+			dp[i][0] = dp[i - 1][1] + arr[i - 1];
+			dp[i][1] = Math.max(dp[i - 2][0], dp[i - 2][1]) + arr[i - 1];
+		}
+
+		System.out.println(Math.max(dp[n][0], dp[n][1]));
+
+		br.close();
+	}
+}
---
+++ backjoon/P_2606.java
@@ -0,0 +1,52 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P_2606 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
+		int[][] g = new int[n][n];
+		for (int i = 0; i < t; i++) {
+			String[] raw = br.readLine().split(" ");
+			int a = Integer.parseInt(raw[0]) - 1;
+			int b = Integer.parseInt(raw[1]) - 1;
+			g[a][b] = 1;
+			g[b][a] = 1;
+		}
+
+		int count = bfs(g, 0);
+
+		System.out.println(count);
+
+		br.close();
+	}
+
+	private static int bfs(int[][] g, int target) {
+		int count = 0;
+		boolean[] v = new boolean[g.length];
+		Queue<Integer> q = new LinkedList<>();
+		q.add(target);
+		v[target] = true;
+
+		while (!q.isEmpty()) {
+			int k = q.poll();
+
+			for (int i = 0; i < g.length; i++) {
+				if (!v[i] && g[k][i] == 1) {
+					q.add(i);
+					v[i] = true;
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+}
