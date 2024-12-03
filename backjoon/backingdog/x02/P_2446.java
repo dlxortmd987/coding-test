d5c6b32e4fa16b3ca9e25ac918bfc47ded68dce0
Taek Seung Lee <dlxortmd987@gmail.com>
2024-12-03 22:59:42 +0900

    바킹독 0x02 - 2446

--- backjoon/backingdog/x02/P_2446.java
+++ backjoon/backingdog/x02/P_2446.java
@@ -3,25 +3,25 @@ package backjoon.backingdog.x02;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class P_2445 {
+public class P_2446 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for (int i = 0; i < 2 * n - 1; i++) {
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < 2 * n; j++) {
-                if (i <= n - 1) {
-                    if (j <= i || (2 * n - i - 1) <= j) {
-                        sb.append("*");
-                    } else {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                if (i < n) {
+                    if (j < i) {
                         sb.append(" ");
+                    } else if (j < 2 * n - 1 - i) {
+                        sb.append("*");
                     }
                 } else {
-                    if (j <= 2 * n - i - 2 || i + 1 <= j) {
-                        sb.append("*");
-                    } else {
+                    if (j < 2 * n - 1 - i - 1) {
                         sb.append(" ");
+                    } else if (j < i + 1) {
+                        sb.append("*");
                     }
                 }
             }
