367fa957dffc0d08ca6d78b45b765034a0dce8cb
Taek Seung Lee <dlxortmd987@gmail.com>
2024-12-25 22:30:51 +0900

    바킹독 0x05 - 1874

--- backjoon/backingdog/x05/P_1874.java
+++ backjoon/backingdog/x05/P_1874.java
@@ -3,28 +3,36 @@ package backjoon.backingdog.x05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class P_1874 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < k; i++) {
-            int n = Integer.parseInt(br.readLine());
 
-            if (n == 0 && !stack.isEmpty()) {
+        int cur = 1;
+        List<String> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            for (int j = cur; j <= k; j++) {
+                stack.push(j);
+                arr.add("+");
+            }
+            if (!stack.isEmpty() && stack.peek() == k) {
+                arr.add("-");
                 stack.pop();
-            } else if (n > 0) {
-                stack.push(n);
+                cur = Math.max(cur, k + 1);
+            } else {
+                System.out.println("NO");
+                return;
             }
         }
-
-        int sum = 0;
-        while (!stack.isEmpty()) {
-            sum += stack.pop();
-        }
-        System.out.println(sum);
+        System.out.println(arr.stream()
+                .collect(Collectors.joining("\n")));
     }
 }
