05fe30044f102aae326ababd03242f156705f2fa
Taek Seung Lee <58359383+dlxortmd987@users.noreply.github.com>
2025-02-07 16:38:04 +0900

    바킹독 0x08 - 3986

--- backjoon/backingdog/x08/P_3986.java
+++ backjoon/backingdog/x08/P_3986.java
@@ -3,12 +3,33 @@ package backjoon.backingdog.x08;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class P_3986 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        int n = Integer.parseInt(br.readLine());
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            char[] line = br.readLine().toCharArray();
+
+            Deque<Character> dq = new LinkedList<Character>();
+            boolean isGood = true;
+            for (char c : line) {
+                if (dq.isEmpty()) {
+                    dq.addFirst(c);
+                } else if (dq.peekFirst() == c) {
+                    dq.pollFirst();
+                } else if (dq.peekFirst() != c) {
+                    dq.addFirst(c);
+                }
+            }
+            if (dq.isEmpty()) {
+                res++;
+            }
+        }
+        System.out.println(res);
     }
 }
